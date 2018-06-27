d988a8764b7608ed397af5bd9bab76bcc42fc887
Rossen_Lenovo_G560 <rossen.halachev@gmail.com>
2018-06-27 15:14:40 +0300

    Synchronising folders

---
+++ src/FinancialUnit/FinancialUnit.java
@@ -0,0 +1,20 @@
+package FinancialUnit;
+
+public class FinancialUnit {
+
+    String name;
+    String adress;
+    int availableCurrency;
+
+    public FinancialUnit(){
+
+    };
+
+    public FinancialUnit(String name, String adress, int availableCurrency) {
+        this.name = name;
+        this.adress = adress;
+        this.availableCurrency = availableCurrency;
+    }
+
+
+}
