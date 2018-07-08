204e225054fcc2386e2968bfaddccab209f71c37
Slavi <slavi_14@abv.bg>
2018-07-08 16:13:41 +0300

    InvestmentServices and BankCards commit.

--- src/bank_product/Main.java
+++ src/bank_product/Main.java
@@ -3,10 +3,11 @@ package bank_product;
 import investment_services_and_activities.CorporateBonds;
 import investment_services_and_activities.GovermentBonds;
 import investment_services_and_activities.Shares;
+import our_exceptions.UnderAgeException;
 import tools.Entity;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws UnderAgeException {
         GovermentBonds govermentBonds = new GovermentBonds("Pesho", 100, 30, 2, 40, 5);
         govermentBonds.buy();
         CorporateBonds corporateBonds=new CorporateBonds("Pesho", 100, 30, 2, 40, 5);
@@ -16,15 +17,15 @@ public class Main {
         share.buy();
         CreditCards creditCards=new CreditCards("P", "8000000000",1000
                 ,40, 1.8,1000);
-        creditCards.clientYearsCheck(2000,1,12);
+        creditCards.checkClientYears(2000,1,12);
         DebitCards debitCards=new DebitCards("P", "8000000000",100
                 ,40,1.8,250);
         debitCards.cardValidityCheck(2018,7,5);
         debitCards.billsPayable(211);
         debitCards.billsPayable(43);
-        corporateBonds.clientYearsCheck(2000,1,21);
+        corporateBonds.checkClientYears(2000,1,21);
         creditCards.cashFromATMWithdraw(20);
-        CurrencyChange clientM=new CurrencyChange(1.69570,AvailableBankCurrencies.BGN,
+        CurrencyChange clientM=new CurrencyChange("Pesho",1.69570,AvailableBankCurrencies.BGN,
                 AvailableBankCurrencies.USD);
         clientM.changeCurrency(100);
     }
